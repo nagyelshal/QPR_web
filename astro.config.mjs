2068130eb841eb5863e6014046089ed0a542def5
Nagy Elshal <nagyelshal@gmail.com>
2025-08-26 15:56:10 -0700

    Fix base path to match repository name QPR_web

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://nagyelshal.github.io',
-  base: '/QPR_Web',
+  base: '/QPR_web',
   output: 'static',
   build: {
     assets: 'assets'
